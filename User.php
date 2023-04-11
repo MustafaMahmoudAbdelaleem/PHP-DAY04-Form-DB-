5112e7fbd362702332afc5d3bcacad823f2c0e7f
Mustafa Mahmoud <124030775+MustafaMahmoudAbdelaleem@users.noreply.github.com>
2023-04-11 08:27:27 +0200

    Update User.php

--- User.php
+++ User.php
@@ -1,147 +1,112 @@
-
 <?php
-$user = 'root';
-$password = '';
- 
-$database = 'phplab04';
- 
-// Server is localhost with
-// port number 3306
-$servername='localhost';
-$mysqli = new mysqli($servername, $user,
-                $password, $database);
- 
-if ($mysqli->connect_error) {
-    die('Connect Error (' .
-    $mysqli->connect_errno . ') '.
-    $mysqli->connect_error);
-}
- 
-$sql = " SELECT * FROM user ";
-$result = $mysqli->query($sql);
-//$mysqli->close();
-?>
+    if(isset($_POST['submit'])){
+        $name = $_POST['userName'];
+        $email = $_POST['userMail'];
+        $gender = $_POST['gender'];
+        if(empty($_POST['emailCheck'])){
+            $check = "no";
+        }else{
+            $check = $_POST['emailCheck'];
+        }
+    }
 
+    // database linking
+
+    $host='localhost';
+    $username = 'root';
+    $password='';
+    $dbname= 'users';
+
+    $link = mysqli_connect($host, $username, $password, $dbname);
+
+    if(!$link){
+        die("Connection failed!" . mysqli_connect_error());
+    }
+    echo "all went good connected to database successfully ...";
+
+    // insert data into from by mysql
+    $mysql = "INSERT INTO user_info(user_name,email,gender,email_check) VALUES ('$name','$email','$gender','$check')";
+
+    $retval = mysqli_query($link , $mysql);
+
+    if(!$retval){
+        die('Could not insert to table: ' . mysqli_error($conn));
+    }else{
+        // construct html to Visualize fetched data
+?>
+<!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link
-			href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-			rel="stylesheet"
-			integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
-			crossorigin="anonymous" />
-		<link rel="stylesheet" href="style.css" />
-		<title>Users Data</title>
-	</head>
-	<body>
-		<div class="container mt-3">
-			<div class="d-flex justify-content-between">
-				<span class="h2">User Details</span>
-				<a class="btn btn-success h-75" href="Form.php" target ="blank" 
-					>Add New User</a
-				>
-			</div>
-			<hr />
-			<table class="table table-striped table-hover">
-				<tr>
-					<th>#</th>
-					<th>Name</th>
-					<th>Email</th>
-					<th>Gender</th>
-					<th>Mail Status</th>
-					<!-- <th>Action</th> -->
-				</tr>
-                <?php
-                // LOOP TILL END OF DATA
-                while($rows=$result->fetch_assoc())
-                {
-            ?>
-            <tr>
-                <!-- FETCHING DATA FROM EACH
-                    ROW OF EVERY COLUMN -->
-                <td><?php echo $rows['user_id'];?></td>
-                <td><?php echo $rows['user_name'];?></td>
-                <td><?php echo $rows['user_email'];?></td>
-                <td><?php echo $rows['gender'];?></td>
-                <td><?php echo $rows['mail_status'];?></td>
-            </tr>
-            <?php
-                }
-            ?>
-			</table>
-		</div>
-		<script
-			src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-			integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-			crossorigin="anonymous"></script>
-	</body>
-</html>
-<!-- ------------------------------------------------------------------------------ -->
-<!-- php Form -->
+<head>      
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <title>Users</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">  
+</head>
+<body>
+    <div class="container mt-5">
+        <header class="my-4">           
+                <div>
+                    <button class="btn btn-success"><a class="nav-link text-light" href="./index.html" target="_blank">Create new user</a></button>
+                </div>            
+        </header>
+        <hr>
+        <div class="row">
+            <table class="table table-hover table-striped">
+                <thead class="table-light">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Gender</th>
+                    <th scope="col">Mail Status</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php         
+                    //establishing connection parameters
+                    $host ='localhost';
+                    $username ='root';
+                    $password ='';
+                    $dbname ='users';
+                
+                    $con = mysqli_connect($host , $username , $password , $dbname);
+                
+                    if(!$con){
+                        die("Connection failed!" . mysqli_connect_error());
+                    }
+                
+                    //fetch and read data from database 
+                
+                    $sql = "SELECT * FROM user_info";
+                
+                    $result = mysqli_query($con , $sql);
+                            
+                    if(mysqli_num_rows($result) > 0){
+                            while($row = mysqli_fetch_assoc($result)){               
+                    echo "
+                            <tr>
+                                <th scope='row'>".$row['id']."</th>
+                                <td>".$row['user_name']."</td>
+                                <td>".$row['email']."</td>
+                                <td>".$row['gender']."</td>
+                                <td>".$row['email_check']."</td>
+                            </tr>
+                        ";
+                            }
+                        }
+                    // close connection
+                    mysqli_close($con);
+                ?>                                      
+                </tbody>
+            </table>
+        </div>
+    </div>    
+</body>
 
 <?php
-$name = $email = $gender = $mailstatus = "";
+    }   
+    // close connection
+    mysqli_close($link);
 
-
-// echo "Name:<br>".$name."<br>";
-// echo "Email:<br>".$email."<br>";
- 
-
-//Open Connection
-$dbhost = 'localhost';
-   $dbuser = 'root';
-   $dbpass = '';
-   $dbname ='phplab';
-   $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
-   
-   if(! $conn ) {
-      die('Could not connect: ' . mysqli_error($conn));
-   }
-   
-   //echo 'Connected successfully<br>';
-
-   //select
-   mysqli_select_db( $conn,$dbname );
-
-  //    $sql = 'CREATE TABLE user( user_id INT NOT NULL AUTO_INCREMENT,
-//    user_name VARCHAR(20) NOT NULL,
-//    user_email  VARCHAR(20) NOT NULL,
-//    gender   INT NOT NULL,
-//    mail_status   INT NOT NULL,
-//    primary key ( user_id ))';
-
-//      $retval = mysqli_query( $conn,$sql );
-   
-//    if(! $retval ) {
-//       die('Could not create table: ' . mysqli_error($conn));
-//    }
-    
-//    echo "<br>Database Table  created successfully\n";
-//    mysqli_close($conn);
-if(!empty($_POST['submit'])){
-  if( isset($_POST["name"]) ||  isset($_POST["email"]) || isset($_POST["gender"])|| 
-  isset($_POST['ckeck'])){
-      $name = $_POST['name'];
-      $email = $_POST['email'];
-      $gender = $_POST['gender'];
-      $mailstatus = filter_input(INPUT_POST,'check', FILTER_VALIDATE_BOOL);
-      $sql = "INSERT INTO user VALUES ('','$name',
-                  '$email','$gender','$mailstatus')";
-
-        // Check if the query is successful
-      if(mysqli_query($conn, $sql)){
-            //echo "<h3>data stored in a database successfully."
-                //
-
-            // echo nl2br("\n$name\n $email\n "
-            //     . "$gender\n $mailstatus\n ");
-        } else{
-            echo "ERROR: Hush! Sorry $sql. "
-                . mysqli_error($conn);
-        }}}
-    // close connection after finishing work
-    mysqli_close($conn);
 ?>
-<!--  -->
